aad387c9afbabb18a1cd16e4dbcef4038273b8b1
jmlake569 <37003520+jmlake569@users.noreply.github.com>
2021-08-24 10:31:32 -0400

    Update list_computer_id.py

--- list_computer_id.py
+++ list_computer_id.py
@@ -1,1 +1,29 @@
+from __future__ import print_function
+import sys, warnings
+import deepsecurity
+from deepsecurity.rest import ApiException
+from pprint import pprint
 
+# Setup
+if not sys.warnoptions:
+	warnings.simplefilter("ignore")
+configuration = deepsecurity.Configuration()
+configuration.host = 'https://cloudone.trendmicro.com/api'
+
+# Authentication
+configuration.api_key['api-secret-key'] = 'YOUR KEY HERE'
+
+# Initialization
+# Set Any Required Values
+api_instance = deepsecurity.ComputersApi(deepsecurity.ApiClient(configuration))
+api_version = 'v1'
+expand_options = deepsecurity.Expand()
+expand_options.add(expand_options.none)
+expand = expand_options.list()
+overrides = False
+
+try:
+	api_response = api_instance.list_computers(api_version, expand=expand, overrides=overrides)
+	pprint(api_response)
+except ApiException as e:
+	print("An exception occurred when calling ComputersApi.list_computers: %s\n" % e)
